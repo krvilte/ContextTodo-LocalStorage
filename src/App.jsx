25eb3a5127d27e16b19a2445a2b4451956895e75
Vishal Kumar Sah <thevishalkumar25@gmail.com>
2024-08-29 00:54:52 +0530

    Assemble Components

--- src/App.jsx
+++ src/App.jsx
@@ -26,10 +26,26 @@ export default function App() {
     setTodos((prev)=> prev.map((prevTodo)=> prevTodo.id === id ? {...prevTodo, complete: !prevTodo.completed} : prevTodo))
   }
   return (
-    <div>
-      <TodoProvider value={{todos, addTodo, editTodo, deleteTodo, todoComplete}}>
-        <TodoForm/>
-      </TodoProvider>
-    </div>
+    <TodoProvider value={{todos, addTodo, editTodo, deleteTodo, todoComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                        <TodoForm />
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                          <div key={todo.id}
+                          className='w-full'
+                          >
+                            <TodoItem todo={todo} />
+                          </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+    </TodoProvider>
   )
 }
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -5,12 +5,13 @@ function TodoForm() {
   const [todo, setTodo] = useState("")
   const {addTodo} = useTodo()
 
-  const add = (e)=>{
-    e.preventDefault();
-    if(!todo)return
-    addTodo({todo, completed: fasle})
-
+  const add = (e) => {
+    e.preventDefault()
+    if (!todo) return
+    addTodo({todo, completed: false})
+    setTodo("")
   }
+
   return (
     <div>
       <form onSubmit={add} action="#" className='flex'>
